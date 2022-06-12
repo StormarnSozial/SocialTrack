36a9acacd3404ec8f6eee0ac48ec4eeb29d753ea
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2022-06-12 15:30:27 +0200

    installed composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'itssebis/profile',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8b92f7f13f4634e24a813bb3a656ed31d6f5b0c1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'itssebis/profile' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8b92f7f13f4634e24a813bb3a656ed31d6f5b0c1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
